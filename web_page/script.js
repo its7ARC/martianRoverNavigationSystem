9bcf48d5a94e41337cc7c110dcfba1e5dae5c364
Abhilasha Bansal <64535297+AbhilashaBansal@users.noreply.github.com>
2021-05-17 01:52:34 +0530

    result display

--- web_page/script.js
+++ web_page/script.js
@@ -1,4 +1,5 @@
 let vertices = [];
+let vert_coor = {};
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
@@ -6,6 +7,10 @@ var ctx = c.getContext("2d");
 let node_no = 1;
 let add_edge = document.getElementById("icon");
 let find_btn = document.getElementById("find");
+let reset_btn = document.getElementById("reset");
+let res_div = document.getElementById("sidediv2");
+$(res_div).hide();
+
 
 // add edge function
 add_edge.onclick = function(){
@@ -29,16 +34,116 @@ add_edge.onclick = function(){
 
     let node4 = document.createElement("input");
     node4.setAttribute("class", "wt");
-    let textnode = document.createTextNode(" "); 
-    let textnode2 = document.createTextNode(" Edge Wt: "); 
+    node4.setAttribute("type", "number");
+
+    let node5 = document.createElement("button");
+    node5.append("Draw");
+    node5.setAttribute("class", "draw_btn");
+
+
     node.appendChild(node2);
-    node.appendChild(textnode);
+    node.append(" ");
     node.appendChild(node3);
-    node.appendChild(textnode2);
+    node.append(" Edge Wt: ");
     node.appendChild(node4);
+    node.append(" ");
+    node.appendChild(node5);
     document.getElementById("edge-ul").appendChild(node);
+
+    // add event listener
+    node5.onclick = function (e){
+        let parent = e.target.parentElement;
+        console.log(parent);
+        let v1 = parent.children[0].value;
+        let v2 = parent.children[1].value;
+        let w3 = parent.children[2].value;
+        console.log(v1, v2, w3);
+        draw_edge(v1, v2, "rgb(20, 89, 146)", w3);
+    }
 }
 
+// draw the edge (event listener)
+function draw_edge(v1, v2, color, wt) {
+    let coor1 = vert_coor[v1];
+    let coor2 = vert_coor[v2];
+
+    ctx.beginPath();
+    ctx.moveTo(coor1[0]+10, coor1[1]);
+    ctx.lineTo(coor2[0]-10, coor2[1]);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = color;
+    ctx.stroke();
+
+    if(wt){
+        draw_label(wt, coor1, coor2);
+    }
+}
+
+function canvas_arrow(v1, v2, color) {
+    ctx.beginPath();
+    let fromx = vert_coor[v1][0];
+    let fromy = vert_coor[v1][1];
+    let tox = vert_coor[v2][0];
+    let toy = vert_coor[v2][1];
+
+    var headlen = 15; // length of head in pixels
+    let arr_x = (fromx+tox)/2;
+    let arr_y = (fromy+toy)/2;
+
+    let dx = tox - fromx;
+    let dy = toy - fromy;
+    // if(arr_x > 40+fromx){
+    //     arr_x -= 20;
+    //     arr_y = (dy/dx)*(arr_x - fromx) + fromy;
+    // }
+    
+    let angle = Math.atan2(dy, dx);
+
+    ctx.moveTo(fromx+10, fromy);
+    ctx.lineTo(tox-10, toy);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = color || "green";
+    
+    
+    ctx.moveTo(arr_x, arr_y);
+    ctx.lineTo(arr_x - headlen * Math.cos(angle - Math.PI / 6), arr_y - headlen * Math.sin(angle - Math.PI / 6));
+    ctx.moveTo(arr_x, arr_y);
+    ctx.lineTo(arr_x - headlen * Math.cos(angle + Math.PI / 6), arr_y - headlen * Math.sin(angle + Math.PI / 6));
+
+    ctx.stroke();
+}
+
+// helper fn, to draw a label
+function draw_label(text, v1, v2){
+    let dx = parseInt(v2[0] - v1[0]);
+    let dy = parseInt(v2[1] - v1[1]-10);  
+    console.log(dx, dy);
+
+    let p=v1;
+    let pad = parseFloat(1/2);
+    let padding = dx/4;
+  
+    pad = padding / Math.sqrt(dx*dx+dy*dy);
+  
+    ctx.save();
+    ctx.textAlign = "left";
+    ctx.translate(parseFloat(p[0] + dx*pad), parseFloat(p[1] + dy*pad));
+    // ctx.rotate(Math.atan2(dy,dx));
+    if(dx<0){
+     ctx.rotate(Math.atan2(dy,dx) - Math.PI);  //to avoid label upside down
+    }
+    else{
+     ctx.rotate(Math.atan2(dy,dx));
+    }
+    ctx.font = '28px arial';
+    ctx.fillStyle = "black"
+    ctx.fillText(text, 0, 0);
+
+    ctx.restore();
+}
+
+
+// click on canvas
 $("#myCanvas").click(function(e){
     getPosition(e);
 
@@ -66,20 +171,22 @@ $("#myCanvas").click(function(e){
 });
 
 
-// canvas functions
+// CANVAS FUNCTIONS
 var pointSize = 20;
 
 function getPosition(event){
     var rect = c.getBoundingClientRect();
     var x = event.clientX - rect.left;
     var y = event.clientY - rect.top;
-        
+
+    let cor = [x, y];
+    vert_coor[node_no] = cor;
+
     drawCoordinates(x,y);
 }
 
 function drawCoordinates(x,y){	
     var ctx = document.getElementById("myCanvas").getContext("2d");
-
 
     ctx.fillStyle = "rgb(37, 75, 156)"; // Blue color
 
@@ -94,9 +201,11 @@ function drawCoordinates(x,y){
     ctx.fillText(node_no, x, y);
 }
 
+
 let graph_vertices = {};
 let graph_edges = {};
 
+// HELPER FN FOR FIND PATH
 function traverse(cur_pos, tc, b, visited, optPaths) {
     if(tc<0) return -2;
 
@@ -137,8 +246,11 @@ function traverse(cur_pos, tc, b, visited, optPaths) {
 
     }
 
-    optPaths[cur_pos]["res"] = maxRes + graph_vertices[cur_pos];
-    temp_path.unshift(cur_pos);
+    if(maxRes != -1){
+        optPaths[cur_pos]["res"] = maxRes + graph_vertices[cur_pos];
+        temp_path.unshift(cur_pos);
+    }
+    
     optPaths[cur_pos]["path"] = temp_path;
     optPaths[cur_pos]["spt"] = maxST;
     // optPaths[cur_pos]["res"] += graph_vertices[cur_pos];
@@ -150,6 +262,8 @@ function traverse(cur_pos, tc, b, visited, optPaths) {
     return optPaths[cur_pos]["res"];
 }
 
+
+// FIND PATH
 find_btn.onclick = function() {
     //process vertices w/ RPs & edges
     // note : Add check condns later
@@ -201,4 +315,56 @@ find_btn.onclick = function() {
     console.log("Max Research Possible:", optPaths[src_vertex]["res"]);
     console.log("Path:", optPaths[src_vertex]["path"]);
     console.log("Spare Time: ", optPaths[src_vertex]["spt"]);
+
+    if(tot_res<0){
+        alert("No path is possible! Its not possible to reach the destination vertex in given conditions! ");
+    }
+
+    else {
+        let path_list = optPaths[src_vertex]["path"];
+        
+        let res_path = document.getElementById("res-path");
+        res_path.innerHTML = "";
+        let i;
+        for(i=0; i<path_list.length-1; i++){
+            canvas_arrow(path_list[i], path_list[i+1], "white");
+            res_path.append(path_list[i] + " --> ");   
+            // try to add gap
+        }
+        res_path.append(path_list[i]);
+
+        document.getElementById("res-mrp").innerHTML = tot_res;
+        document.getElementById("res-mst").innerHTML = optPaths[src_vertex]["spt"];
+        
+
+        $(res_div).show();
+    }
+}
+
+
+// RESET FN
+reset_btn.onclick = function (){
+    let ele = document.getElementById("res-pot");
+    while(ele.firstChild){
+        ele.removeChild(ele.lastChild);
+    }
+
+    let ele_2 = document.getElementById("edge-ul");
+    while(ele_2.firstChild){
+        ele_2.removeChild(ele_2.lastChild);
+    }
+
+    vertices = [];
+    vert_coor = {};
+    node_no = 1;
+    graph_edges = {};
+    graph_vertices = {};
+    $(res_div).hide();
+
+    let sel1 = document.getElementById("srcv");
+    let sel2 = document.getElementById("destv");
+    $(sel1).empty();
+    $(sel2).empty();
+
+    ctx.clearRect(0, 0, c.width, c.height);
 }
